90c38677a0add43a86af6f355e96f761d62e3934
Sebastian Leszman <sebastianleszman@gmail.com>
2020-04-16 18:50:21 +0200

    another commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// gets the appropriate content for a given fragment identifier
+const getContent = (fragmentId) => {
+  fetch(`${fragmentId}.html`)
+    .then((res) => res.text())
+    .then((data) => {
+      const contentDiv = document.querySelector("#content");
+      contentDiv.innerHTML = data;
+    })
+    .catch((err) => console.log("Error: ", err));
+};
+
+// set the 'active' class on the active navigation link
+const setActiveLink = (fragmentId) => {
+  const navbarLinks = document.querySelectorAll("#navbar a");
+  navbarLinks.forEach((link) => {
+    if (fragmentId === link.getAttribute("href").substr(1)) {
+      link.setAttribute("class", "active");
+    } else {
+      link.removeAttribute("class");
+    }
+  });
+};
+
+// updates dynamic content based on the fragment identifier
+const navigate = () => {
+  const fragmentId = location.hash.substr(1);
+  getContent(fragmentId);
+  // toggle the 'active' class on the currently navigated link
+  setActiveLink(fragmentId);
+};
+
+// if no fragment identifier is provide
+if (!location.hash) location.hash = "#home";
+
+navigate();
+
+addEventListener("hashchange", navigate);
